e29e92833a238e951de488ab6a31f21f2c1204df
Aditya-221 <47222312+Aditya-221@users.noreply.github.com>
2020-01-02 20:24:41 +0530

    New file is added which is the framework of main page of the software

---
+++ Main_page.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# window creation
+win = TK()
+win.title("Happy Coding")
+win.configure(background="white")
+
+create label
+
+Lable (win, text="Enter any text: ", bg = "white", fg = "green", font = "none 12 bold").grid(row=1, column=0, sticky=E)
